af488d6fbd084b0ce4de33b233aad897f3a552eb
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-19 03:30:17 +0300

    Initial commit

---
+++ src/test/java/com/willis/quiz_service/QuizServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willis.quiz_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
