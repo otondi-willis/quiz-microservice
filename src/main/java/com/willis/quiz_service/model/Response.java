85d6b1dbfac369d24c4ee9938d7bbbfbde15737c
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-19 03:41:32 +0300

    model: added Response model

---
+++ src/main/java/com/willis/quiz_service/model/Response.java
@@ -0,0 +1,11 @@
+package com.willis.quiz_service.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Response {
+    private Integer id;
+    private String response;
+}
