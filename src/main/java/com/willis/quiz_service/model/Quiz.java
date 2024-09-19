4d5d3c51f46db8b904068f87fed2b28802d08113
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-19 03:40:59 +0300

    model: added Quiz model

---
+++ src/main/java/com/willis/quiz_service/model/Quiz.java
@@ -0,0 +1,17 @@
+package com.willis.quiz_service.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    @ManyToMany
+    private List<Question> questions;
+}
