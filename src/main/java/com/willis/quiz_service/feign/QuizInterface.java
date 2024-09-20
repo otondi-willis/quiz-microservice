e01ec4d22304ae043e9ad20b5f17b2453ae96e80
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-20 03:35:07 +0300

    Feign: added three methods declaration

--- src/main/java/com/willis/quiz_service/feign/QuizInterface.java
+++ src/main/java/com/willis/quiz_service/feign/QuizInterface.java
@@ -1,7 +1,26 @@
 package com.willis.quiz_service.feign;
 
+import com.willis.quiz_service.model.QuestionWrapper;
+import com.willis.quiz_service.model.Response;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @FeignClient("QUESTION-SERVICE")
 public interface QuizInterface {
+    @GetMapping("generate")
+    public ResponseEntity<List<Integer>> getQuestionsForQuiz(@RequestParam String categoryName, @RequestParam Integer numQuestions);
+
+
+    //getQuestions (questionId)
+    @PostMapping("getQuestions")
+    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(@RequestBody List<Integer> questionIds);
+    //getScore
+    @PostMapping("getScore")
+    public ResponseEntity<Integer> getScore(@RequestBody List<Response> responses);
 }
