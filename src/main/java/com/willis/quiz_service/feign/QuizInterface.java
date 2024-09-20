0a5f2ca4a10cd1cbccadd8ceddb506ef9e03920d
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-20 03:25:52 +0300

    Package: Added new package entailing new Quiz Interface

---
+++ src/main/java/com/willis/quiz_service/feign/QuizInterface.java
@@ -0,0 +1,7 @@
+package com.willis.quiz_service.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient
+public interface QuizInterface {
+}
--- src/main/java/com/willis/quiz_service/service/QuizService.java
+++ src/main/java/com/willis/quiz_service/service/QuizService.java
@@ -2,7 +2,6 @@ package com.willis.quiz_service.service;
 
 
 import com.willis.quiz_service.dao.QuizDao;
-import com.willis.quiz_service.model.Question;
 import com.willis.quiz_service.model.QuestionWrapper;
 import com.willis.quiz_service.model.Quiz;
 import com.willis.quiz_service.model.Response;
@@ -23,7 +22,8 @@ public class QuizService {
 //    QuestionDao questionDao;
 
     public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
-//        List<Question> questions = questionDao.findRandomQuestionsByCategory(category);
+
+  //      List<Integer> questions = //call the generate url - RestTemplate http://localhost:8080/question/generate
 //
 //        // Ensure we only return up to numQ questions
 //        if (questions.size() < numQ) {
@@ -45,14 +45,14 @@ public class QuizService {
     }
 
     public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
-        Optional<Quiz> quiz = quizDao.findById(id);
-        List<Question> questionsFromDB = quiz.get().getQuestions();
+//        Optional<Quiz> quiz = quizDao.findById(id);
+//        List<Question> questionsFromDB = quiz.get().getQuestions();
         List<QuestionWrapper> questionsForUser = new ArrayList<>();
-        for(Question q : questionsFromDB){
-            QuestionWrapper qw = new QuestionWrapper(q.getId(),q.getQuestiontitle(),q.getOption1(),q.getOption2(),q.getOption3(),q.getOption4());
-
-            questionsForUser.add(qw);
-        }
+//        for(Question q : questionsFromDB){
+//            QuestionWrapper qw = new QuestionWrapper(q.getId(),q.getQuestiontitle(),q.getOption1(),q.getOption2(),q.getOption3(),q.getOption4());
+//
+//            questionsForUser.add(qw);
+//        }
 
         return new ResponseEntity<>(questionsForUser,HttpStatus.OK);
 
@@ -61,14 +61,14 @@ public class QuizService {
 
     public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
         Quiz quiz = quizDao.findById(id).get();
-        List<Question> questions = quiz.getQuestions();
-        int right = 0;
-        int i= 0;
-        for(Response response:responses){
-            if(response.getResponse().equals(questions.get(i).getRightanswer()))
-                right++;
-            i++;
-        }
+//        List<Question> questions = quiz.getQuestions();
+       int right = 0;
+//        int i= 0;
+//        for(Response response:responses){
+//            if(response.getResponse().equals(questions.get(i).getRightanswer()))
+//                right++;
+//            i++;
+//        }
         return new ResponseEntity<>(right,HttpStatus.OK);
     }
 }
